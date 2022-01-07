fd2be909c4359a79ed859acc564155b6d93fbd89
RenatoLima0 <renatoslima46@gmail.com>
2022-01-07 15:41:54 -0300

    formulários trial e fit

---
+++ assets/js/main/main.js
@@ -0,0 +1,20 @@
+const activeForms = document.querySelector('.main-forms .btn-active-forms');
+const initialForms = document.querySelector('.initial-forms');
+const secForms = document.querySelector('section.section');
+const finishForms = document.querySelector('.cta.btn-finish');
+const secThanks = document.querySelector('section.sec-thanks');
+
+const activeFormsClick = (e) => {
+  e.preventDefault();
+  initialForms.classList.remove('active');
+  secForms.classList.add('active');
+}
+
+const finishFormsFunc = (e) => {
+  e.preventDefault();
+  secForms.classList.remove('active');
+  secThanks.classList.add('active');
+}
+
+activeForms.addEventListener('click', activeFormsClick);
+finishForms.addEventListener('click', finishFormsFunc);

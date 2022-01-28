515832940985137871b7e2c9f73a1871b48a65aa
Paulo Vaz <pvaz100@gmail.com>
2022-01-28 01:42:48 -0300

    feat: pages

---
+++ assets/js/utils/tab.js
@@ -0,0 +1,8 @@
+function tabLink(click, box) {
+  const dataClick = document.querySelectorAll(click)
+  const dataBox = document.querySelectorAll(box)
+
+  console.log('dataClick', dataClick)
+  console.log('dataBox', dataBox)
+  
+}

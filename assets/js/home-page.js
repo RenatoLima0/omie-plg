b2715cf0c3ba5545bf7d3cf14875b7244846ecd6
RenatoLima0 <renatoslima46@gmail.com>
2022-02-25 17:02:22 -0300

    novo banner home

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,78 +1,78 @@
-if(document.querySelector('[data-banner]')) {
-  const bannerHome = document.querySelector('[data-banner]');
-  const title = bannerHome.querySelector('.nav-banner .titles-banner .title-1');
-  const titleText = bannerHome.querySelector('.nav-banner .titles-banner p');
-  const namePersona = bannerHome.querySelector('.nav-banner .name');
-  const role = bannerHome.querySelector('.nav-banner .role');
-  const bullets = bannerHome.querySelectorAll('.bullet');
-  const arrows = bannerHome.querySelectorAll('.arrows [data-arrow]');
+// if(document.querySelector('[data-banner]')) {
+//   const bannerHome = document.querySelector('[data-banner]');
+//   const title = bannerHome.querySelector('.nav-banner .titles-banner .title-1');
+//   const titleText = bannerHome.querySelector('.nav-banner .titles-banner p');
+//   const namePersona = bannerHome.querySelector('.nav-banner .name');
+//   const role = bannerHome.querySelector('.nav-banner .role');
+//   const bullets = bannerHome.querySelectorAll('.bullet');
+//   const arrows = bannerHome.querySelectorAll('.arrows [data-arrow]');
   
-  const limpaBullets = () => {
-    bullets.forEach(bullet => bullet.classList.remove('ativo'));
-  }
+//   const limpaBullets = () => {
+//     bullets.forEach(bullet => bullet.classList.remove('ativo'));
+//   }
   
-  const changeBanner = (value) => {
-    switch (value) {
-      case 0:
-        bannerHome.classList.remove('banner-3');
-        bannerHome.classList.add('banner-1');
+//   const changeBanner = (value) => {
+//     switch (value) {
+//       case 0:
+//         bannerHome.classList.remove('banner-3');
+//         bannerHome.classList.add('banner-1');
   
-        title.classList = 'title-1';
-        titleText.innerText = 'Aprimorou suas habilidades de gestão através dos cursos da Omie.Academy';
-        namePersona.innerText = 'Luana Santana';
-        limpaBullets();
-        bullets[0].classList.add('ativo');
-        // role.innerText = 'Empresária Omie';
+//         title.classList = 'title-1';
+//         titleText.innerText = 'Aprimorou suas habilidades de gestão através dos cursos da Omie.Academy';
+//         namePersona.innerText = 'Luana Santana';
+//         limpaBullets();
+//         bullets[0].classList.add('ativo');
+//         // role.innerText = 'Empresária Omie';
   
-        break;
+//         break;
   
-      case 1:
-        bannerHome.classList.remove('banner-1');
-        bannerHome.classList.add('banner-2');
-        title.classList = 'title-2';
-        titleText.innerText = 'Investiu no sistema de gestão da Omie e viu sua empresa crescer rapidamente';
-        namePersona.innerText = 'Maria Oliveira';
-        limpaBullets();
-        bullets[1].classList.add('ativo');
-        break;
+//       case 1:
+//         bannerHome.classList.remove('banner-1');
+//         bannerHome.classList.add('banner-2');
+//         title.classList = 'title-2';
+//         titleText.innerText = 'Investiu no sistema de gestão da Omie e viu sua empresa crescer rapidamente';
+//         namePersona.innerText = 'Maria Oliveira';
+//         limpaBullets();
+//         bullets[1].classList.add('ativo');
+//         break;
   
-      case 2:
-        bannerHome.classList.remove('banner-2');
-        bannerHome.classList.add('banner-3');
-        title.classList = 'title-3';
-        titleText.innerText = 'Se tornou um Franqueado Omie e em pouco tempo viu seus lucros aumentarem';
-        namePersona.innerText = 'Renan Gomes';
-        limpaBullets();
-        bullets[2].classList.add('ativo');
-        break;
+//       case 2:
+//         bannerHome.classList.remove('banner-2');
+//         bannerHome.classList.add('banner-3');
+//         title.classList = 'title-3';
+//         titleText.innerText = 'Se tornou um Franqueado Omie e em pouco tempo viu seus lucros aumentarem';
+//         namePersona.innerText = 'Renan Gomes';
+//         limpaBullets();
+//         bullets[2].classList.add('ativo');
+//         break;
   
-      default:
-        break;
-    }
-  }
+//       default:
+//         break;
+//     }
+//   }
   
-  const numArray = [0, 1, 2];
-  const rodaBanner = setInterval(callback, 7000);
+//   const numArray = [0, 1, 2];
+//   const rodaBanner = setInterval(callback, 7000);
 
-  function callback() {
-    let primeiro = numArray.shift();
-    changeBanner(primeiro);
-    numArray.push(primeiro);
-  }
+//   function callback() {
+//     let primeiro = numArray.shift();
+//     changeBanner(primeiro);
+//     numArray.push(primeiro);
+//   }
 
-  const carouselBannerHome = (e) => {
-    clearInterval(rodaBanner);
-    let primeiro = numArray.shift();
-    changeBanner(primeiro);
-    numArray.push(primeiro);
-  }
+//   const carouselBannerHome = (e) => {
+//     clearInterval(rodaBanner);
+//     let primeiro = numArray.shift();
+//     changeBanner(primeiro);
+//     numArray.push(primeiro);
+//   }
 
-  arrows.forEach(arrow => {
-    arrow.addEventListener('click', carouselBannerHome)
-  })
-}
+//   arrows.forEach(arrow => {
+//     arrow.addEventListener('click', carouselBannerHome)
+//   })
+// }
 
-if(document.querySelector('.col-contabilidade')){
+if(document.querySelector('.col-contabilidade') && tela >= 1024){
   const colContabilidade = document.querySelector('.col-contabilidade');
   const rowSegmentos = document.querySelector('.row-segmentos');
 
